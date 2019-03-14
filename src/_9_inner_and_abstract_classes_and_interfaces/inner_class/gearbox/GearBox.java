b2c430ac5b0a62cb9d56421968a1fe4c7804b22b
Patryk077 <p.pawlina@poczta.onet.pl>
2019-03-14 23:33:11 +0100

    Inner class

---
+++ src/_9_inner_and_abstract_classes_and_interfaces/inner_class/gearbox/GearBox.java
@@ -0,0 +1,66 @@
+package _9_inner_and_abstract_classes_and_interfaces.inner_class.gearbox;
+
+import java.util.ArrayList;
+
+public class GearBox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+    private boolean clutchIsIn;
+
+    public GearBox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+
+        for (int i = 0; i < maxGears; i++) {
+            addGear(i, i * 5.3);
+        }
+    }
+
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if ((number > 0) && (number <= maxGears))
+            this.gears.add(new Gear(number, ratio));
+    }
+
+    public void changeGear(int newGear) {
+        if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Gggrrrr!!!!" + "\n" + "The Gear is not change "
+                    + " your current gear is " + this.currentGear);
+        }
+    }
+
+    public double speedWheel(int revs) {
+        if (clutchIsIn) {
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+    public class Gear {
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
+        }
+
+        public double driveSpeed(int revs) {
+            return revs * this.ratio;
+        }
+    }
+}
---
+++ src/_9_inner_and_abstract_classes_and_interfaces/inner_class/gearbox/Main.java
@@ -0,0 +1,21 @@
+package _9_inner_and_abstract_classes_and_interfaces.inner_class.gearbox;
+
+public class Main {
+    public static void main(String[] args) {
+        GearBox mcLaren = new GearBox(6);
+//        GearBox.Gear first = mcLaren.new Gear(1,12.3);    declaration of interface the inner class
+//        GearBox.Gear second = mcLaren.new Gear(2,12.6);
+//        System.out.println(first.driveSpeed(1000)+second.driveSpeed(1550));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.speedWheel(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.speedWheel(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.speedWheel(6000));
+    }
+}
+
